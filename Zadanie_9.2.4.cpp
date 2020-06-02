a7909dbfc0811ef5a64140c710a1b1723ca04819
Oleksandr Bilan <sasha.bilan.5558@gmail.com>
2020-06-02 20:20:49 +0300

    Uvolnenie vsetkej pamati, ktora sa podiela na reprezentacii matici

--- Zadanie_9.2.4.cpp
+++ Zadanie_9.2.4.cpp
@@ -2,41 +2,47 @@
 #include<stdlib.h>
 #include<math.h>
 #include<time.h>
-
+ 
 typedef struct
 {
-	unsigned int size;
-	float *udiag;
-	float *diag;
-	float *ldiag;
-	
+    unsigned int size;
+    float *udiag;
+    float *diag;
+    float *ldiag;
+    
 } TDMAT;
-
+ 
 TDMAT *tdmat_create_with_type(unsigned int size)
 {
-
-	TDMAT* t = (TDMAT*)malloc(sizeof(TDMAT)*size);
-	
-	if (t == NULL)
-	{
+    TDMAT* t = (TDMAT*)malloc(sizeof(float)*size);
+    
+    if(t == NULL)
+    {
 		free(t); //Funkcia uvolnuje pamat
 		return NULL; //Funkcia vracia nulu
 	}
 	else
 	{
-		printf("Adresa struktury TDMAT: %d", t); //Funkcia vypysuje adresu struktury TDMAT
-		return t; //Funkcia vracia adresu struktury TDMAT
+    	return t; //Funkcia vracia adresu struktury TDMAT
 	}
 }
-
+ 
 void tdmat_destroy(TDMAT *mat)
 {
+	if (mat != NULL)
+	
+	free(mat); //Funkcia uvolnuje vsetku pamat, ktora sa podiela na reprezentacii tridiagonalnej matice
 	
 }
-
+ 
 int main(void)
 {
-	unsigned int size = 5;
-	
-	tdmat_create_with_type(size);
+    unsigned int size = 5;
+    TDMAT* c;
+    
+    c = tdmat_create_with_type(size);
+    printf("%d", c);
+    
+    tdmat_destroy(c);
+
 }
