6ef90e47e5a38239ebce2e193e95adca28d9a740
Oleksandr Bilan <sasha.bilan.5558@gmail.com>
2020-06-03 17:15:39 +0300

    Priraduje vsetkym diagonalnym elementam jednotky, a mimodiagonalnym nulu

--- Zadanie_9.2.4.cpp
+++ Zadanie_9.2.4.cpp
@@ -14,35 +14,61 @@ typedef struct
  
 TDMAT *tdmat_create_with_type(unsigned int size)
 {
-    TDMAT* t = (TDMAT*)malloc(sizeof(float)*size);
+    TDMAT* t = (TDMAT*)malloc(sizeof(float)*size*size);
     
     if(t == NULL)
     {
-		free(t); //Funkcia uvolnuje pamat
-		return NULL; //Funkcia vracia nulu
+		free(t);
+		return NULL;
 	}
 	else
 	{
-    	return t; //Funkcia vracia adresu struktury TDMAT
+    	return t;
 	}
 }
  
 void tdmat_destroy(TDMAT *mat)
-{
-	if (mat != NULL)
-	
-	free(mat); //Funkcia uvolnuje vsetku pamat, ktora sa podiela na reprezentacii tridiagonalnej matice
-	
+{	
+	free(mat);
 }
- 
-int main(void)
+
+void tdmat_unit(TDMAT *mat)
 {
-    unsigned int size = 5;
+	int i;
+	float e[mat->size], f[mat->size], g[mat->size];
+
+	mat->udiag = e;
+	mat->diag = f;
+	mat->ldiag = g;
+	
+	for(i = 0; i < mat->size; i++)
+	{
+    	mat->udiag[i] = 0;
+    	printf("%f ", mat->udiag[i]);
+
+    	mat->diag[i] = 1;
+    	printf("%f ", mat->diag[i]);
+    
+    	mat->ldiag[i] = 0;
+    	printf("%f ", mat->ldiag[i]);
+    }
+}
+
+
+int main()
+{
+	unsigned int a;
+	
     TDMAT* c;
     
-    c = tdmat_create_with_type(size);
-    printf("%d", c);
+    printf("Zadaj rozmier matice: ");
+	scanf("%d", &a);
     
-    tdmat_destroy(c);
+    c = tdmat_create_with_type(a);
+    
+    c->size = a;
+    
+    tdmat_unit(c);
 
+    tdmat_destroy(c);
 }
