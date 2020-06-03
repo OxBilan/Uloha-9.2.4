db56cce482b0337342f2bfef8e25281321fabecb
Oleksandr Bilan <sasha.bilan.5558@gmail.com>
2020-06-03 19:42:03 +0300

    Opravene vypisovanie tridiagonalnej matice

--- Zadanie_9.2.4.cpp
+++ Zadanie_9.2.4.cpp
@@ -34,48 +34,86 @@ void tdmat_destroy(TDMAT *mat)
 
 void tdmat_unit(TDMAT *mat)
 {
-	int i;
-	float e[mat->size], f[mat->size], g[mat->size];
-
-	mat->udiag = e;
-	mat->diag = f;
-	mat->ldiag = g;
+	int i, j, k;
 	
-	for(i = 0; i < mat->size; i++)
+	for(i = 0; i < mat->size-1; i++)
 	{
     	mat->udiag[i] = 0;
-    	printf("%f ", mat->udiag[i]);
-
-    	mat->diag[i] = 1;
-    	printf("%f ", mat->diag[i]);
-    
-    	mat->ldiag[i] = 0;
-    	printf("%f ", mat->ldiag[i]);
-    }
-    printf("\n");
+	}
+	
+	for(j = 0; j < mat->size; j++)
+	{
+    	mat->diag[j] = 1;
+	}
+	
+	for(k = 0; k < mat->size-1; k++)
+	{
+    	mat->ldiag[k] = 0;
+	}
 }
 
 void tdmat_random(TDMAT *mat)
 {
-	int i;
-	float b[mat->size], c[mat->size], d[mat->size];
-
-	mat->udiag = b;
-	mat->diag = c;
-	mat->ldiag = d;
+	int i, j, k;
 	
-	for(i = 0; i < mat->size; i++)
+	for(i = 0; i < mat->size-1; i++)
 	{
     	mat->udiag[i] = (rand()/(float)(RAND_MAX)) * 2 - 1;
-    	printf("%f ", mat->udiag[i]);
+	}
+	
+	for(j = 0; j < mat->size; j++)
+	{
+    	mat->diag[j] = (rand()/(float)(RAND_MAX)) * 2 - 1;
+	}
+	
+	for(k = 0; k < mat->size-1; k++)
+	{
+    	mat->ldiag[k] = (rand()/(float)(RAND_MAX)) * 2 - 1;
+	}
+}
 
-    	mat->diag[i] = (rand()/(float)(RAND_MAX)) * 2 - 1;
-    	printf("%f ", mat->diag[i]);
+void tdmat_print(TDMAT *mat)
+{
+	int i, j, k, l, m, n;
+	
+	tdmat_unit(mat);
     
-    	mat->ldiag[i] = (rand()/(float)(RAND_MAX)) * 2 - 1;
-    	printf("%f ", mat->ldiag[i]);
+	for(i = 0; i < mat->size-1; i++)
+	{
+    	printf("%f ", mat->udiag[i]);
+	}
+	printf("\n");
+	
+    for(j = 0; j < mat->size; j++)
+	{
+    	printf("%f ", mat->diag[j]);
+	}
+	printf("\n");
+	
+    for(k = 0; k < mat->size-1; k++)
+	{
+    	printf("%f ", mat->ldiag[k]);
+    }
+    printf("\n\n");
+    
+    tdmat_random(mat);
+    
+    for(l = 0; l < mat->size-1; l++)
+	{
+    	printf("%f ", mat->udiag[l]);
+	}
+	printf("\n");
+	
+	for(m = 0; m < mat->size; m++)
+	{
+    	printf("%f ", mat->diag[m]);
     }
     printf("\n");
+    
+	for(n = 0; n < mat->size-1; n++)
+	{
+    	printf("%f ", mat->ldiag[n]);
+    }
 }
 
 int main()
@@ -84,16 +122,20 @@ int main()
 	
     TDMAT* c;
     
-    printf("Zadaj rozmier matice: ");
+    printf("Zadaj velkost matice: ");
 	scanf("%d", &a);
     
     c = tdmat_create_with_type(a);
     
     c->size = a;
     
-    tdmat_unit(c);
+    float e[c->size-1], f[c->size], g[c->size-1];
+
+	c->udiag = e;
+	c->diag = f;
+	c->ldiag = g;
     
-    tdmat_random(c);
+    tdmat_print(c);
     
     tdmat_destroy(c);
 }
